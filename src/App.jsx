720b801598ee29f0915a1dd5d06b59315038dfe6
alex.pinaida <alex.pinaida@rappi.com>
2024-11-08 09:58:28 -0500

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Contador es {count}
+          Contador es hola mundo {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
